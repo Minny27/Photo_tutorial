150c5f9e2aadcc19c08ad9d92225f7e1ce8afc7c
Minny27 <dltmdals0608@naver.com>
2021-04-13 15:49:14 +0900

    Initial Commit

---
+++ Photo_tutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo_tutorial
+//
+//  Created by SeungMin on 2021/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
